5aed11227bcb4ee0abb42ded030cd741ed7b2d4b
jzplp <jzplp@qq.com>
2019-04-22 20:29:36 +0800

    1.3-1.8

---
+++ Chapter-1/1.3.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main()
+{
+	std::cout<<"hello, World";
+	return 0;
+}
---
+++ Chapter-1/1.7.cpp
@@ -0,0 +1,11 @@
+// 根据题目要求，这是错误的代码 
+#include<iostream>
+int main()
+{
+	/* 这是注释
+	 * /*   this is notes */
+ 	 *
+	 *
+	 */ 
+	return 0;
+}
