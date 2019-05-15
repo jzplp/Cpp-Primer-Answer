755a7bb7efc62273e56be21c813798bcf1f4dda6
jzplp <jzplp@qq.com>
2019-05-15 14:42:14 +0800

    2.13-2.24

---
+++ Chapter-2/2.18.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+int main()
+{
+	int i1 = 1, i2 = 2;
+	int * p1 = &i1;
+	// 更改指针的值 
+	p1 = &i2;
+	// 更改指针所指对象的值
+	*p1 = 3;
+	std::cout << i1 << " " << i2 << std::endl; 
+	return 0;
+}
